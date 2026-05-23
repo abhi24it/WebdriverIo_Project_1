8c38c8c471dcd8bfdeac0e8fca41ffbd45e0fabf
abhisheksharma <abhisharma4it@gmail.com>
2026-05-23 23:33:52 +0530

    adding login Page

--- project-health-management/test/pageobjects/LoginPage.js
+++ project-health-management/test/pageobjects/LoginPage.js
@@ -1,2 +1,23 @@
-class LoginPage {}
+class LoginPage {
+	get inputUsername() { return $('#username'); }
+	get inputPassword() { return $('#password'); }
+	get btnLogin() { return $('button[type="submit"]'); }
+	get errorMessage() { return $('.error, .error-message'); }
+
+	open() {
+		return browser.url('/login');
+	}
+
+	async login(username, password) {
+		await (await this.inputUsername).setValue(username);
+		await (await this.inputPassword).setValue(password);
+		await (await this.btnLogin).click();
+	}
+
+	async getErrorText() {
+		const el = await this.errorMessage;
+		return el.isExisting() ? el.getText() : '';
+	}
+}
+
 module.exports = new LoginPage();
